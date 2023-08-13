928affe561d91cf2b19c88fac945aedfb58c7972
RazaAli313 <136004622+RazaAli313@users.noreply.github.com>
2023-08-13 15:55:46 +0500

    Create BBC.clone.js

---
+++ BBC.clone.js
@@ -0,0 +1,7 @@
+
+    var currentDate = new Date();
+    var dateElement = document.getElementById('date');
+    dateElement.innerHTML = currentDate.toDateString();
+    window.addEventListener("load",function(){
+         window.alert("Welcome to BBC.com");
+    })
